ea83df4285eaf95ef4878c397c4e93aa67af5bb9
jakopako <jakob.dhondt@web.de>
2025-05-01 22:17:21 +0200

    cleanup + battery low detection

--- arduino/lumaire/lumaire.ino
+++ arduino/lumaire/lumaire.ino
@@ -7,27 +7,15 @@ The FFT part is heavily based on https://github.com/kosme/arduinoFFT/blob/master
 */
 
 /*
-currents (LEDs prob at 3.3V, prob more current at higher voltage):
-- sleep + light off: 0.33mA
-- ardiuno on + light off: 1.34mA
-- arduino on + light max (6 neo pixels): 6.7mA
-- sleep + light max: 5.68mA
-
-with 1500mAH battery:
-- max time: 4545.45 hours
-- min time: 223.88 hours
-
-
-12/04/2025
+1/5/2025
 currents:
-- sleep + light off: works but not when connected to the computer.. hughhh?? 0.52mA
-- ardiuno on + light off: 1.6mA
-- arduino on + light max (10 neo pixels): 16.7mA
-- sleep + light max: works but not when connected to the computer.. hughhh??
+- sleep + light off: 0.37mA
+- arduino on + light off: 1.47mA
+- arduino on + light max (brightness 100, rgb 120,neo pixel jewel): 6.2mA
 
 with 1500mAH battery:
-- max time: 937.5 hours
-- min time: 89.82 hours
+- max time: 4054 hours
+- min time: 241.9 hours
 */
 
 #include "arduinoFFT.h"
@@ -37,27 +25,24 @@ with 1500mAH battery:
 #include "BlowClassifier.h"
 #include "SuckClassifier.h"
 
-const int CHANNEL = A0;
+// analog inputs
+const int MIC = A0;
 const int VBATTERY = A2;
 
-bool DEBUG = true;
+// toggles
+bool DEBUG = false;
 bool TRAIN = false;
-bool SLEEP = false;
+bool SLEEP = true;
 
 // How sensitive to be to changes in voltage for waking up
-const int margin = 400;
+const int wakeUpMargin = 400;
+
+// sampling
 const uint16_t samples = 128;           // This value MUST ALWAYS be a power of 2
 const double samplingFrequency = 5000;  // Hz, must be less than 10000 due to ADC
 unsigned int sampling_period_us;
 unsigned long microseconds;
-const int led_pin_neo = 3;
-const int max_brightness = 100;
-int num_pixels = 10;
-int neo_brightness = 0;
-const int button_pin = 2;
-int button_state = 0;
 int repetitions = 500;  // 2300 rep. is roughly a minute worth of looping with 5000 Hz sampling and 128 samples per loop
-
 /*
 These are the input and output vectors
 Input vectors receive computed results from FFT
@@ -65,25 +50,36 @@ Input vectors receive computed results from FFT
 double vReal[samples];
 double vImag[samples];
 
+// NeoPixel
+const int led_pin_neo = 3;
+const int max_brightness = 100;
+int num_pixels = 10;
+int neo_brightness = 0;
+
+// training button
+const int button_pin = 2;
+int button_state = 0;
+
+// battery
+const float battery_threshold = 3.5;
+
+
 /* Create FFT object */
 ArduinoFFT<double> FFT = ArduinoFFT<double>(vReal, vImag, samples, samplingFrequency);
 
-Adafruit_DotStar strip = Adafruit_DotStar(1, INTERNAL_DS_DATA, INTERNAL_DS_CLK, DOTSTAR_BGR);  // build-in led to show sleep/wake state
-Adafruit_NeoPixel pixels(num_pixels, led_pin_neo, NEO_GRB + NEO_KHZ800);
-// Argument 1 = Number of pixels in NeoPixel strip
-// Argument 2 = Arduino pin number (most are valid)
+Adafruit_DotStar strip = Adafruit_DotStar(1, INTERNAL_DS_DATA, INTERNAL_DS_CLK, DOTSTAR_BGR);  // build-in led to show sleep/wake state on Trinket m0
 // Argument 3 = Pixel type flags, add together as needed:
 //   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel pixels(num_pixels, led_pin_neo, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  // buil-in pixel on Trinket m0
   strip.begin();
   strip.setPixelColor(0, 0xFF0000);  // red
-  if (SLEEP){
+  if (SLEEP) {
     strip.setBrightness(1);
   } else {
     strip.setBrightness(0);
@@ -97,7 +93,7 @@ void setup() {
   pixels.show();
 
   sampling_period_us = round(1000000 * (1.0 / samplingFrequency));
-  pinMode(CHANNEL, INPUT);
+  pinMode(MIC, INPUT);
   pinMode(VBATTERY, INPUT);
   pinMode(button_pin, INPUT);
   if (DEBUG || TRAIN) {
@@ -119,22 +115,17 @@ void processAudio() {
   int pred = 1;
   int prevPred = 1;
   for (int r = 0; r < repetitions; r++) {
-    // need to do 1:2 resistor ratio because vMax > 3.3V, e.g.
-    // bat - 10kO - VBATTERY - 20kO - GND
-    int bat = analogRead(VBATTERY);
-    float vBat = 5 * (bat / 1023.0);
-    serialLog("Bat: ");
-    serialLogln(vBat);
-
-    /*SAMPLING*/
+    // SAMPLING
     microseconds = micros();
     for (int i = 0; i < samples; i++) {
-      vReal[i] = analogRead(CHANNEL);
+      vReal[i] = analogRead(MIC);
       vImag[i] = 0;
       while (micros() - microseconds < sampling_period_us)
         ;
       microseconds += sampling_period_us;
     }
+
+    // FFT
     FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward); /* Weigh data */
     FFT.compute(FFTDirection::Forward);                       /* Compute FFT */
     FFT.complexToMagnitude();
@@ -145,6 +136,7 @@ void processAudio() {
       magnitude[i] = (float)vReal[i];
     }
 
+    // for generating a training set
     printLabeledData(magnitude, mag_len, 1);
 
     // Label mapping{'blow' : 0, 'nope' : 1, 'suck' : 2}
@@ -153,9 +145,13 @@ void processAudio() {
     serialLog("prediction: ");
     serialLogln(pred);
     if (pred == 0 && prevPred == 0) {
-      // TODO if battery low -> blink led and return
-      increaseBrightnessNeoPixels();
-      serialLogln("led on");
+      if (batteryLow()) {
+        blinkNeoPixels();
+        return;
+      } else {
+        increaseBrightnessNeoPixels();
+        serialLogln("led on");
+      }
     }
 
     if (pred == 2) {
@@ -165,17 +161,33 @@ void processAudio() {
   }
 }
 
+bool batteryLow() {
+  // need to do 1:2 resistor ratio because vMax > 3.3V
+  // and the board I'm currently using has 3.3V pins (Trinket M0), e.g.
+  // bat - 10kO - VBATTERY - 20kO - GND
+  int bat = analogRead(VBATTERY);
+  float vBat = 4.95 * (bat / 1023.0);
+  serialLog("Bat: ");
+  serialLog(vBat);
+  if (vBat < battery_threshold) {
+    serialLogln(" - low");
+    return true;
+  }
+  serialLogln(" - high");
+  return false;
+}
+
 void goToSleep() {
   serialLogln("going to sleep");
   strip.setBrightness(0);
   strip.show();
   // Read the voltage at the ADC pin
-  int value = analogRead(CHANNEL);
+  int value = analogRead(MIC);
 
   // Define a window around that value
-  uint16_t lo = max(value - margin, 0);
-  uint16_t hi = min(value + margin, UINT16_MAX);
-  LowPower.attachAdcInterrupt(CHANNEL, wakeUp, ADC_INT_OUTSIDE, lo, hi);
+  uint16_t lo = max(value - wakeUpMargin, 0);
+  uint16_t hi = min(value + wakeUpMargin, UINT16_MAX);
+  LowPower.attachAdcInterrupt(MIC, wakeUp, ADC_INT_OUTSIDE, lo, hi);
   LowPower.sleep();
   LowPower.detachAdcInterrupt();
   strip.setBrightness(1);
@@ -254,33 +266,6 @@ void printLabeledData(float *magnitude, int mag_len, int mode) {
   }
 }
 
-void serialLogln(const char *input) {
-  if (DEBUG) {
-    Serial.println(input);
-  }
-}
-
-void serialLogln(const double input) {
-  if (DEBUG) {
-    Serial.println(input);
-  }
-}
-
-void serialLog(const char *input) {
-  if (DEBUG) {
-    Serial.print(input);
-  }
-}
-
-void serialLog(const double input) {
-  if (DEBUG) {
-    Serial.print(input);
-  }
-}
-
-void wakeUp() {
-}
-
 void increaseBrightnessNeoPixels() {
   int max = 120;
   int diff = 10;
@@ -306,4 +291,46 @@ void turnOffNeoPixels() {
   neo_brightness = 0;
   pixels.clear();
   pixels.show();
+}
+
+void blinkNeoPixels() {
+  neo_brightness = 100;
+  uint32_t color = pixels.Color(neo_brightness, neo_brightness, neo_brightness, neo_brightness);
+  serialLog("Blink brightness :");
+  serialLogln(neo_brightness);
+  for (int i = 0; i < 3; i++) {
+    pixels.fill(color);
+    pixels.show();
+    delay(500);
+    pixels.clear();
+    pixels.show();
+    delay(500);
+  }
+}
+
+void serialLogln(const char *input) {
+  if (DEBUG) {
+    Serial.println(input);
+  }
+}
+
+void serialLogln(const double input) {
+  if (DEBUG) {
+    Serial.println(input);
+  }
+}
+
+void serialLog(const char *input) {
+  if (DEBUG) {
+    Serial.print(input);
+  }
+}
+
+void serialLog(const double input) {
+  if (DEBUG) {
+    Serial.print(input);
+  }
+}
+
+void wakeUp() {
 }
